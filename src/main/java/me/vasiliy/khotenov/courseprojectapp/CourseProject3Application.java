474d41ccd8915f829dc81078149dcbe4758cae63
Пользователь <woodoo08@mail.ru>
2023-01-21 20:45:10 +0300

    first commit

---
+++ src/main/java/me/vasiliy/khotenov/courseprojectapp/CourseProject3Application.java
@@ -0,0 +1,13 @@
+package me.vasiliy.khotenov.courseprojectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProject3Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/vasiliy/khotenov/courseprojectapp/CourseProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.vasiliy.khotenov.courseprojectapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
